80c3727e1ad50b410cb7207636f88c35f336cb31
Ehckme <ehckme@gmail.com>
2023-11-30 15:26:27 +0200

    fouth-update

--- Sign_Up.py
+++ Sign_Up.py
@@ -4,13 +4,15 @@ from flask import Flask
 from flask import render_template, url_for, session
 from flask import request, redirect
 from sqlalchemy import String, Integer, CHAR
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import Session, sessionmaker
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.types import Boolean
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
 import dns.resolver
 
 # import flask login
@@ -33,6 +35,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/New
 # config secrete key
 app.config['SECRET_KEY'] = 'TooTopSecrete'
 
+# add bcrypt for hashing
+
+
 engine = create_engine('mysql+pymysql://root:root@localhost/New_User_Database')
 sess = Session(engine)
 # initialize database app
@@ -54,7 +59,7 @@ class Users(db.Model, UserMixin):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(30))
-    password: Mapped[str] = mapped_column(CHAR(80))
+    password: Mapped[str] = mapped_column(CHAR(200))
     confirmed: Mapped[bool] = mapped_column(nullable=False, default=False)
     otp: Mapped[int] = mapped_column(nullable=True)
 
@@ -76,6 +81,7 @@ def load_user(user_id):
 
 
 # view route page for login
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     mail = request.form.get('email', False)
@@ -83,15 +89,19 @@ def login():
     if request.method == 'POST':
         valid_user_email = Users.query.filter_by(email=mail).first_or_404()
         if valid_user_email:
-            valid_password = Users.query.filter_by(password=password).first_or_404()
-            if valid_password:
-                is_confirmed = valid_user_email.confirmed
-                if is_confirmed == True:
-                    login_user(valid_user_email)
-                    print(is_confirmed)
-                    return redirect(url_for('dashboard', user_username=valid_user_email.username))
-                else:
-                    return redirect(url_for('confirm'))
+            ph = PasswordHasher()
+            hash = valid_user_email.password
+            try:
+                if ph.verify(hash, password):
+                    is_confirmed = valid_user_email.confirmed
+                    if is_confirmed == True:
+                        login_user(valid_user_email)
+                        print(is_confirmed)
+                        return redirect(url_for('dashboard', user_username=valid_user_email.username))
+                    else:
+                        return redirect(url_for('confirm'))
+            except VerifyMismatchError:
+                return redirect(url_for('login'))
 
                 # return render_template('dashboard.html', user_name=Valid_user_email.username)
 
@@ -119,13 +129,16 @@ OTP = random_otp()
 def sign_up():
     user_email = request.form.get('email', False)
     user_name = request.form.get('username', False)
+    pass_word = request.form.get('password', False)
 
     session['username'] = request.form.get('username', False)
     if request.method == 'POST':
+        ph = PasswordHasher()
+        hash = ph.hash(pass_word)
         new_user = Users(
             username=request.form.get('username', False),
             email=request.form.get('email', False),
-            password=request.form.get('password', False),
+            password=hash,
         )
 
         email_in_database = Users.query.filter_by(email=user_email).first()
@@ -194,6 +207,7 @@ def confirm():
         if user_input_otp != OTP:
 
             print('Invalid OTP pin')
+            print('Your OTP is: ', OTP)
         elif user_input_otp == OTP:
             print('Your OTP is: ', OTP)
             if 'username' in session:
@@ -208,8 +222,104 @@ def confirm():
     return render_template('confirm.html')
 
 
+@app.route('/account_recovery', methods=['GET', 'POST'])
+def account_recovery():
+    email_to_recover = request.form.get('email', False)
+    check_recovery_email = Users.query.filter_by(email=email_to_recover).first()
+
+    session['user_from_email'] = check_recovery_email.username
+
+    if request.method == 'POST':
+        if check_recovery_email:
+
+            """ Split domain from user_email input"""
+            domain = email_to_recover.split('@')[1]
+
+            """ Check the existence of domain
+                and catch  the error with try-except method. 
+            """
+            try:
+                dns_address = dns.resolver.resolve(f'{domain}', 'A')
+                for dns_a in dns_address:
+                    print('A record : ', dns_a.to_text())
+
+                    # send email
+                    import smtplib
+                    from email.mime.multipart import MIMEMultipart
+                    from email.mime.text import MIMEText
+
+                    message = MIMEMultipart()
+                    """ Send SMTP message to user_email input """
+                    message['To'] = email_to_recover
+                    message['From'] = 'ehckmedev@gmail.com'
+                    message['Subject'] = 'CONFIRM'
+
+                    title = '<b> CONFIRM OTP-CODE </b>'
+
+                    messageText = MIMEText(
+                        f''' Hello {check_recovery_email.username}, your OTP-Code is: {OTP}  ''', 'html'
+                    )
+                    message.attach(messageText)
+
+                    """ Sender infor email and app password """
+                    sender_email = 'ehckmedev@gmail.com'
+                    app_password = 'iyjf yndm yoxb qysh'
+
+                    server = smtplib.SMTP('smtp.gmail.com:587')
+                    server.ehlo('Gmail')
+                    server.starttls()
+                    server.login(sender_email, app_password)
+
+                    fromaddr = sender_email
+                    toaddr = email_to_recover
+                    server.sendmail(fromaddr, toaddr, message.as_string())
+                    server.quit()
+
+                    return redirect(url_for('otp_reset',))
+
+            except dns.exception.DNSException:
+                print('Invalid Email')
+
+            print(email_to_recover)
+
+            return redirect(url_for('otp_reset'))
+        else:
+            print('Email does not exist')
+    return render_template('account_recovery.html')
+
+
+@app.route('/otp_reset', methods=['GET', 'POST'])
+def otp_reset():
+    if request.method == 'POST':
+        user_input_otp = request.form.get('otp', False)
+        if user_input_otp != OTP:
+
+            print('Invalid OTP pin')
+        elif user_input_otp == OTP:
+            print('Your OTP is: ', OTP)
+            return redirect(url_for('reset_password',))
+
+    return render_template('otp_reset.html')
+
+
 @app.route('/reset_password', methods=['GET', 'POST'])
 def reset_password():
+    if request.method == 'POST':
+        new_password = request.form.get('new_password', False)
+        confirm_new_password = request.form.get('confirm_new_password', False)
+        if new_password == confirm_new_password:
+            if 'user_from_email' in session:
+                ph = PasswordHasher()
+                hash = ph.hash(confirm_new_password)
+                user_from_email = session['user_from_email']
+                print(user_from_email)
+                user_reset_pw = Users.query.filter_by(username=user_from_email).first()
+                user_reset_pw.password = hash
+                db.session.commit()
+            return redirect(url_for('login', email=user_from_email))
+        else:
+            print('Password do not match')
+
     return render_template('reset_password.html')
 
 
