36ffc33f04c71f6a2c2b31df2d350a3aab303b19
Ehckme <ehckme@gmail.com>
2023-12-02 02:30:32 +0200

    fifth update

--- Sign_Up.py
+++ Sign_Up.py
@@ -2,7 +2,7 @@ import flask
 import requests
 from flask import Flask
 from flask import render_template, url_for, session
-from flask import request, redirect
+from flask import request, redirect, flash
 from sqlalchemy import String, Integer, CHAR
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
@@ -22,6 +22,7 @@ from flask_login import UserMixin, LoginManager, login_required, login_user, log
 # create an inheritance class from DeclarativeBase
 class Base(DeclarativeBase):
     pass
+
 
 sql_session = sessionmaker()
 # instan
@@ -48,13 +49,19 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+""" Load user from login_manager with their user id. 
+"""
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Users.query.get(int(user_id))
+    return db.session.get(Users, int(user_id))
 
 
-# Create a Table class using flask-sqlaclhemy
+""" Create a User Model, which is a table for Users with flask-sqlalchemy 
+"""
+
+
 class Users(db.Model, UserMixin):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
@@ -67,89 +74,134 @@ class Users(db.Model, UserMixin):
 with app.app_context():
     db.create_all()
 
+""" view route for home page.  
+"""
 
-# view route page for home
+
 @app.route('/', methods=['GET'])
+@login_required
 def home():
-    return render_template('login.html')
+    return render_template('dashboard.html')
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return db.session.get(Users, int(user_id))
-    # return Users.query.get(int(user_id))
+""" view route for login page.  
+"""
 
-
-# view route page for login
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Get user email and password from the user input from the form
     mail = request.form.get('email', False)
     password = request.form.get('password', False)
+
     if request.method == 'POST':
-        valid_user_email = Users.query.filter_by(email=mail).first_or_404()
+
+        # Validate the database email with the user input email
+        valid_user_email = Users.query.filter_by(email=mail).first()
+
         if valid_user_email:
+
+            # instantiate PasswordHasher from argon2-cffi
             ph = PasswordHasher()
             hash = valid_user_email.password
+
+            # Use the try block to catch the exception
             try:
+                # Verify the hash in database with the user input password
                 if ph.verify(hash, password):
+
+                    # Check if user has confirmed email in database
                     is_confirmed = valid_user_email.confirmed
+
                     if is_confirmed == True:
+
+                        # Login the user
                         login_user(valid_user_email)
+
+                        # Display flash message
+                        flash(f'You are logged in {valid_user_email.username}')
                         print(is_confirmed)
                         return redirect(url_for('dashboard', user_username=valid_user_email.username))
                     else:
                         return redirect(url_for('confirm'))
             except VerifyMismatchError:
-                return redirect(url_for('login'))
 
-                # return render_template('dashboard.html', user_name=Valid_user_email.username)
+                # Display a flash message
+                flash('please enter correct password')
+                return redirect(url_for('login'))
+        else:
+
+            # Display a flash message
+            flash('please enter correct email')
 
     return render_template('login.html')
 
 
-# view route page for Dashboard
+""" view route for dashboard page.  
+"""
+
+
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-
     return render_template('dashboard.html')
 
 
-""" create OTP code """
+""" create OTP code to use for confirming the user
+    by importing and using the random module
+
+"""
 import random
+
+
 def random_otp():
     otp = ''.join([str(random.randint(0000, 9999)) for i in range(1)])
     return otp
+
+
 OTP = random_otp()
 
+""" view route for sign_up page  
+"""
 
-# view route page for sign up
+
 @app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
+    # Get user email, username and password from the user input from the form
     user_email = request.form.get('email', False)
     user_name = request.form.get('username', False)
     pass_word = request.form.get('password', False)
 
+    # create a session containing the user's username
     session['username'] = request.form.get('username', False)
+
     if request.method == 'POST':
+
+        # instantiate PasswordHasher from argon2-cffi
         ph = PasswordHasher()
         hash = ph.hash(pass_word)
+
+        # Get user input details from the form to add them to database
         new_user = Users(
             username=request.form.get('username', False),
             email=request.form.get('email', False),
             password=hash,
         )
 
+        # Check if the email entered is in the database
         email_in_database = Users.query.filter_by(email=user_email).first()
+
         if email_in_database:
+            # Display flash message
+            flash('Email already exists. ')
             print('Email already exists')
             return redirect(url_for('sign_up'))
         else:
+
             """ Split domain from user_email input"""
             domain = user_email.split('@')[1]
 
-            """ Check the existence of domain
+            """ Check the existence of DNS domain
                 and catch  the error with try-except method. 
             """
             try:
@@ -159,7 +211,10 @@ def sign_up():
                     db.session.add(new_user)
                     db.session.commit()
 
-                    # send email
+                    ###############################################################
+                    ### The following is a block for sending email with smtplib ###
+                    ###############################################################
+
                     import smtplib
                     from email.mime.multipart import MIMEMultipart
                     from email.mime.text import MIMEText
@@ -195,9 +250,13 @@ def sign_up():
 
             except dns.exception.DNSException:
                 print('Invalid Email')
-                return redirect(url_for('sign_up'))
+                return redirect(url_for('sign_up',))
 
-    return render_template('sign_up.html')
+    return render_template('sign_up.html',)
+
+
+""" view route for confirm page  
+"""
 
 
 @app.route('/confirm', methods=['GET', 'POST'])
@@ -222,11 +281,16 @@ def confirm():
     return render_template('confirm.html')
 
 
+""" view route for account_recovery page  
+"""
+
+
 @app.route('/account_recovery', methods=['GET', 'POST'])
 def account_recovery():
     email_to_recover = request.form.get('email', False)
     check_recovery_email = Users.query.filter_by(email=email_to_recover).first()
 
+    # create a session containing the user's username from email
     session['user_from_email'] = check_recovery_email.username
 
     if request.method == 'POST':
@@ -235,7 +299,7 @@ def account_recovery():
             """ Split domain from user_email input"""
             domain = email_to_recover.split('@')[1]
 
-            """ Check the existence of domain
+            """ Check the existence of DNS domain
                 and catch  the error with try-except method. 
             """
             try:
@@ -243,6 +307,9 @@ def account_recovery():
                 for dns_a in dns_address:
                     print('A record : ', dns_a.to_text())
 
+                    ###############################################################
+                    ### The following is a block for sending email with smtplib ###
+                    ###############################################################
                     # send email
                     import smtplib
                     from email.mime.multipart import MIMEMultipart
@@ -261,7 +328,7 @@ def account_recovery():
                     )
                     message.attach(messageText)
 
-                    """ Sender infor email and app password """
+                    """ Sender info email and app password """
                     sender_email = 'ehckmedev@gmail.com'
                     app_password = 'iyjf yndm yoxb qysh'
 
@@ -275,7 +342,7 @@ def account_recovery():
                     server.sendmail(fromaddr, toaddr, message.as_string())
                     server.quit()
 
-                    return redirect(url_for('otp_reset',))
+                    return redirect(url_for('otp_reset', ))
 
             except dns.exception.DNSException:
                 print('Invalid Email')
@@ -288,25 +355,36 @@ def account_recovery():
     return render_template('account_recovery.html')
 
 
+""" view route for otp_reset page  
+"""
+
+
 @app.route('/otp_reset', methods=['GET', 'POST'])
 def otp_reset():
     if request.method == 'POST':
         user_input_otp = request.form.get('otp', False)
         if user_input_otp != OTP:
 
-            print('Invalid OTP pin')
+            print('Invalid OTP code')
+            flash('Invalid OTP code')
         elif user_input_otp == OTP:
             print('Your OTP is: ', OTP)
-            return redirect(url_for('reset_password',))
+            return redirect(url_for('reset_password', ))
 
     return render_template('otp_reset.html')
+
+
+""" view route for reset_password page  
+"""
 
 
 @app.route('/reset_password', methods=['GET', 'POST'])
 def reset_password():
     if request.method == 'POST':
+        # Get user's new password, and confirm password from the user input from the form
         new_password = request.form.get('new_password', False)
         confirm_new_password = request.form.get('confirm_new_password', False)
+
         if new_password == confirm_new_password:
             if 'user_from_email' in session:
                 ph = PasswordHasher()
@@ -319,8 +397,13 @@ def reset_password():
             return redirect(url_for('login', email=user_from_email))
         else:
             print('Password do not match')
+            flash('Password do not match')
 
     return render_template('reset_password.html')
+
+
+""" view route for logout page  
+"""
 
 
 @app.route('/logout', methods=['GET', 'POST'])
@@ -332,4 +415,3 @@ def logout():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
